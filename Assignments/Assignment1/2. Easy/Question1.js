056d8830366d8ffd65766f5d02ecc758ed842470
Akash Kumar <akashkrcse1@gmail.com>
2020-08-14 11:09:17 +0530

    Update :  easy question 1 completed

---
+++ Assignments/Assignment1/2. Easy/Question1.js
@@ -0,0 +1,16 @@
+function charCount (ch,arr) 
+{
+    var count = 0;
+    for (var i=0;i<arr.length;i++) 
+    {
+        if (arr.charAt(i) == ch) 
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+console.log(charCount ("a", "edabit"));
+console.log(charCount  ("c", "Chamber of secrets"));
+console.log(charCount  ("b", "big fat bubble"));
