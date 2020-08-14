a559de9a2ddf6b4ee596a513d02c21d01f8cbb27
Akash Kumar <akashkrcse1@gmail.com>
2020-08-14 13:43:57 +0530

    Completed task

---
+++ Assignments/Assignment1/2. Easy/Question2.js
@@ -0,0 +1,15 @@
+console.log("Easy - Q2. Add up the Numbers from a Single Number");
+
+function addUp (num) 
+{
+ var sum = 0;
+ for (var i=1;i<=num;i++) 
+ {
+    sum=sum+i;
+  }
+  return sum;
+}
+
+console.log(addUp(4));
+console.log(addUp(13));
+console.log(addUp(600));
